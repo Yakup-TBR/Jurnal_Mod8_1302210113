038f80eaac8ba09aeafeb155afc2de4a0f4134ea
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-04-10 09:17:36 +0800

    p

---
+++ modul8_1302210113/BankTransferConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul8_1302210113
+{
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; } = "en";
+
+        public int threshold { get; set; } = 25000000;
+
+        public int low_fee { get; set; } = 6500;
+
+        public int high_fee { get; set; } = 15000;
+
+        //public string methods { get; set; } = "RTO (real-time)", "SKN", "RTGS", "BI FAST"]; //kurung siku blom
+
+        public string en { get; set; } = "yes";
+
+        public string id { get; set; } = "ya";
+
+        public void translate()
+        {
+            if(lang == "en")
+            {
+                this.lang = "Please insert the amount of money to transfer: ";
+                
+            }
+            else if(lang == "id")
+            {
+                lang = "Masukkan jumlah uang yang akan di transfer: ";
+            }
+            Console.WriteLine(lang);
+        }
+    }
+}
--- modul8_1302210113/Program.cs
+++ modul8_1302210113/Program.cs
@@ -1,2 +1,69 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using modul8_1302210113;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+class Program
+{
+    private static string json;
+
+    static void Main()
+    {
+        try
+        {
+            json = File.ReadAllText(@"C:\Users\Yakup Asmaidy\OneDrive\Dokumen\Semester_4\Kontruksi Perangkat Lunak\modul8_1302210113\modul8_1302210113\bank_transfer_config.json");
+
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("");
+        }
+        BankTransferConfig config = JsonConvert.DeserializeObject<BankTransferConfig>(json);
+        config.translate();
+
+        int angka;
+        
+        while (!int.TryParse(Console.ReadLine(), out angka))
+        {
+            Console.Write("Masukkan invalid, ulangi : ");
+
+        }
+
+        
+        if(angka <= config.threshold)
+        {
+            Console.WriteLine($"Total Biaya Transfer  {config.low_fee}" );
+        }
+
+        else if(angka >= config.threshold)
+        {
+            Console.WriteLine($"Total Biaya Transfer  {config.high_fee}");
+        }
+        
+
+
+        /*
+        if (config.threshold <= angka)
+        {
+
+        }
+        */
+
+        /*
+        if (config.lang == "en")
+        {
+            Console.WriteLine("P");
+            //###############
+        }
+        */
+
+
+
+
+    }
+}
+
